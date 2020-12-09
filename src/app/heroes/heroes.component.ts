eb3c79a6e84b3b39e913518ab6c0b1ed6701bf05
Jérôme Haas <j.haas@cyberplanet.ch>
2020-12-09 18:45:36 +0100

    Add service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,9 +2,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
-
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -19,16 +19,21 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }
   
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   ngOnInit() {
-     
+    this.getHeroes();
   }
 
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
+}
+
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 
 }
